9566767aeb352d0f733372097d5ec15ad79a456f
unknown <30691@fiecalunos.lan>
2019-11-08 22:46:48 -0300

    Adicionado o DAO e banco de dados

--- models/orcamentoDAO.js
+++ models/orcamentoDAO.js
@@ -1,6 +1,14 @@
+
+// const con = require('../helpers/connection')
 class OrcamentoDAO {
 
-
+// buscaPorUsuarioESenha(usuario, cb){
+//     const sql = "select * from tb_usuarios where rm=? and senha=?";
+//     con.query(sql, [usuario.rm, usuario.senha], (err,res) => {
+//          if(err) throw err;
+//          else cb(res);
+//     });
+// }
 
 }
 module.exports = OrcamentoDAO;
