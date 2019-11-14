65b50cc32e4882d4d840bf69e0cb6319d9cb5bcd
unknown <30683@fiecalunos.lan>
2019-11-13 22:55:10 -0300

    Exemplo de autenticação

--- config/express.js
+++ config/express.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
 const cors = require('cors');
 const path = require('path');
 
@@ -8,10 +9,18 @@ module.exports = function () {
     const app = express();
 
     app.use(cors());
+    app.use(cookieParser())
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
 
     // ROUTES //
+   // app.get('/oi', (req, res) => {
+      //  if(req.cookies.token){
+        //    res.redirect('/views/cadastro')
+        //} else {
+        //    res.send('nop')
+        // }
+    //})
 
     app.get('/views/cadastro', function (req, res) {
         res.sendfile(path.resolve(__dirname + '/../public/cadastro.html'));
