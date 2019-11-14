11c7bbd692a30f51c0b567e3b24cbf758b592c3a
unknown <30684@fiecalunos.lan>
2019-11-14 14:16:05 -0300

    2

--- helpers/connection.js
+++ helpers/connection.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
-const connection  = mysql.createConnection({
-    host : 'localhost',
-    username : 'root',
-    password : '1234',
-    database : 'cgo'
+const connection = mysql.createConnection({
+    host: 'localhost',
+    username: 'root',
+    password: 'fiec@1234',
+    database: 'cgo'
 });
 
 module.exports = connection;
