b61e8582f26f9db53ef1cae9bfdc7310da52306f
unknown <30684@fiecalunos.lan>
2019-11-22 18:01:35 -0300

    ..

--- config/express.js
+++ config/express.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const nodemailer = require('nodemailer');
 
 const rotasDeOrcamentos = require('../routes/orcamentos')
 const rotasDePlantas = require('../routes/plantas')
@@ -9,18 +10,20 @@ const rotasDeUsuarios = require('../routes/usuarios')
 
 
 
- const cookieParser = require('cookie-parser')
- 
+const cookieParser = require('cookie-parser')
+
 
 module.exports = function () {
     const app = express();
 
-    
+
     app.use(cors());
-    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.urlencoded({
+        extended: false
+    }));
     app.use(bodyParser.json());
     app.use(cookieParser())
-    
+
     const indexRoute = require('../routes/index')
     app.use('/', indexRoute)
 
@@ -28,6 +31,33 @@ module.exports = function () {
     app.use('/plantas', rotasDePlantas);
     app.use('/componentes', rotasDeComponentes);
     app.use('/usuarios', rotasDeUsuarios);
+
+
+    // var transporter = nodemailer.createTransport({
+    //     service: 'gmail',
+    //     port: 587,
+    //     auth: {
+    //         user: 'cgocontato2965@gmail.com',
+    //         pass: 'EcGo2965'
+    //     }
+    // });
+
+    // var mailOptions = {
+    //     from: 'cgocontato2965@gmail.com',
+    //     to: 'cassielrattes@gmail.com',
+    //     subject: 'Sending Email using Node.js',
+    //     text: 'That was easy!'
+    // };
+
+    // transporter.sendMail(mailOptions, function (error, info) {
+    //     if (error) {
+    //         console.log(error);
+    //     } else {
+    //         console.log('Email sent: ' + info.response);
+    //     }
+    // });
+
+
 
 
     return app;
