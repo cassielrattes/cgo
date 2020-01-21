7fccb95cc655f40aea10317fb3da04be5a44a1fa
Cassiel Rattes Cortez <52519353+cassielrattes@users.noreply.github.com>
2020-01-20 21:55:54 -0300

    Update express.js

--- config/express.js
+++ config/express.js
@@ -1,21 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const nodemailer = require('nodemailer');
+const cookieParser = require('cookie-parser')
 
 const rotasDeOrcamentos = require('../routes/orcamentos')
 const rotasDePlantas = require('../routes/plantas')
 const rotasDeComponentes = require('../routes/componentes')
 const rotasDeUsuarios = require('../routes/usuarios')
 
-
-
-const cookieParser = require('cookie-parser')
-
-
 module.exports = function () {
     const app = express();
-
 
     app.use(cors());
     app.use(bodyParser.urlencoded({
@@ -31,34 +25,6 @@ module.exports = function () {
     app.use('/plantas', rotasDePlantas);
     app.use('/componentes', rotasDeComponentes);
     app.use('/usuarios', rotasDeUsuarios);
-
-
-    // var transporter = nodemailer.createTransport({
-    //     service: 'gmail',
-    //     port: 587,
-    //     auth: {
-    //         user: 'cgocontato2965@gmail.com',
-    //         pass: 'EcGo2965'
-    //     }
-    // });
-
-    // var mailOptions = {
-    //     from: 'cgocontato2965@gmail.com',
-    //     to: 'cassielrattes@gmail.com',
-    //     subject: 'Sending Email using Node.js',
-    //     text: 'That was easy!'
-    // };
-
-    // transporter.sendMail(mailOptions, function (error, info) {
-    //     if (error) {
-    //         console.log(error);
-    //     } else {
-    //         console.log('Email sent: ' + info.response);
-    //     }
-    // });
-
-
-
 
     return app;
 }
